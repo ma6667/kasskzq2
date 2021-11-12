4d8d557cd869ace33bce38dabda4b2e2ba932639
ma6667 <mamingjun6667@gmail.com>
2021-11-12 14:35:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kasskzq/dieryemian.java
@@ -0,0 +1,14 @@
+package com.example.kasskzq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dieryemian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dieryemian);
+    }
+}
---
+++ app/src/main/java/com/example/kasskzq/disanyemian.java
@@ -0,0 +1,14 @@
+package com.example.kasskzq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class disanyemian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disanyemian);
+    }
+}
